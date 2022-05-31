66ed7748ed0a801394f775f32e3b1e1948f30c8d
caixiangfan <caixiang@ualberta.ca>
2022-05-31 21:56:16 +0000

    added 11-lottery

---
+++ 11-Lottery/Project/tests/Lottery.ts
@@ -0,0 +1,22 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { Lottery } from "../typechain";
+
+function convertStringArrayToBytes32(array: string[]) {
+    const bytes32Array = [];
+    for (let index = 0; index < array.length; index++) {
+      bytes32Array.push(ethers.utils.formatBytes32String(array[index]));
+    }
+    return bytes32Array;
+}
+
+describe("Lottery", function () {
+    let lotteryContract: Lottery;
+    let lotteryFactory: any;
+    let tokenContractFactory: any;
+    // let tokenContract: MyToken;
+    // let accounts: SignerWithAddress[];
+    it("test lottery", async () => {
+      
+    })
+})
